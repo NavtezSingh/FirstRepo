eac3701c4610fa575829b7822cb2fb70802d466b
NavtezSingh <navtezsingh09@gmail.com>
2023-09-15 13:39:34 +0530

    First file added moj karo

---
+++ MyProgram.c
@@ -0,0 +1,9 @@
+#include<stdio.h>// Preprocessor directives _______ Header file   or <stdlib.h>
+
+int main() // exactly 1 main function
+{
+	printf("Hello World\n"); // printf is also a function with no output 
+	system("pause"); // to pause the output screen or else it would disappear 
+	// getchar() can also be used to stop just to get one character input 
+	return 0;
+}
